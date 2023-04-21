8f0cf0adbda06af568483f42986644c705328692
Marwa Abouafia <marwaabouafia@gmail.com>
2023-04-21 09:56:40 -0700

    ss

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l != 'e' && l != 'q')
+			putchar(l);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
