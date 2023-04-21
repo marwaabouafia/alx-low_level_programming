353cc426b5bfb4b85e67219886accfd37e6c8b96
Marwa Abouafia <marwaabouafia@gmail.com>
2023-04-21 09:32:15 -0700

    kk

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha '\n');
+
+	return (0);
+}
