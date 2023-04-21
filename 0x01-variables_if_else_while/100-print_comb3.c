d7f0d3c13a122ab88411f69a85bf233aa3dd2f8d
Marwa Abouafia <marwaabouafia@gmail.com>
2023-04-21 12:19:27 -0700

    s

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - a simple program that outputs 0-9 separated by commas
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
