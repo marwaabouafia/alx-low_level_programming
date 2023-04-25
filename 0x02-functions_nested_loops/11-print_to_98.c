e69e4dcbe15473be025280d0fedc377d83c5fd16
Marwa Abouafia <marwaabouafia@gmail.com>
2023-04-25 15:37:02 -0700

    l

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,12 +13,12 @@ void print_to_98(int n)
 	if (n < 98)
 	{
 		for (i = n; i < 98; i++)
-			printf("%d,", i);
+			printf("%d, ", i);
 	}
 	else
 	{
 		for (i = n; i > 98; i--)
-			printf("%d,", i);
+			printf("%d, ", i);
 	}
 	printf("98\n");
 }
