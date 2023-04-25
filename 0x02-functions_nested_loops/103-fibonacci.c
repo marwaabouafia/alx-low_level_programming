a96384efb85b9f0b27b47dc0416e5e82e43b2076
Marwa Abouafia <marwaabouafia@gmail.com>
2023-04-25 16:26:35 -0700

    l

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	float tsum;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+		if (sum > 4000000)
+			break;
+		if (sum % 2 == 0)
+			tsum = tsum + sum;
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%f\n", tsum);
+	return (0);
+}
+
