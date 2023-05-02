0af602954bb852fdf3542419631d320aaaa135e0
Marwa Abouafia <marwaabouafia@gmail.com>
2023-05-02 16:07:24 -0700

    d

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *  _atoi - a function that convert a string to an integer.
+ *
+ *  @s: char
+ *
+ *  Return: always 0
+ *
+ */
+
+int _atoi(char *s)
+{
+	int n, i;
+
+	n = 0;
+	i = 1;
+
+	do {
+		if (*s == '-')
+			i *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+
+	return (n * s);
+}
