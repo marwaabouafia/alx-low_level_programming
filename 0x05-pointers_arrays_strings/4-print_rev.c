51aa5f0f4bd934488203f2cba3b8dde8774733af
Marwa Abouafia <marwaabouafia@gmail.com>
2023-05-02 14:19:26 -0700

    l

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse,
+ *
+ * @s: char
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	while (i--)
+	{
+		_putchar (s[i]);
+	}
+	_putchar ('\n');
+}
