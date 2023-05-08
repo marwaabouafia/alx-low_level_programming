ab7b33762dea54aceef0c661f31edfa72ae6075f
Marwa Abouafia <marwaabouafia@gmail.com>
2023-05-08 11:09:24 -0700

    d

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function that prints the chessboard.
+ *
+ * @a: an 8 by 8 matrix
+ *
+ * Return: void
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 7)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
