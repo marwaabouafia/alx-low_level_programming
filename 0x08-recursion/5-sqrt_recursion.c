89efacc790e4c1cefd34ac6e4be83ad5e524a01c
Marwa Abouafia <marwaabouafia@gmail.com>
2023-05-10 14:08:35 -0700

    d

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,12 +2,23 @@
 
 /**
  * _sqrt_recursion - unction that returns the natural square root of a number.
+ * _my_sqrt - returns the natural square root of a number
  *
  * @n: int
+ * @root: root
  *
  * Return: square root
  *
  */
+int _my_sqrt(int n, int root)
+{
+	if (root * root > n)
+		return (-1);
+	if (root * root == n)
+		return (root);
+	return (_my_sqrt(n, ++root));
+}
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
