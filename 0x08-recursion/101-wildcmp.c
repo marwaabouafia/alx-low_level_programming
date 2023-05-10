b8c429c1d0db6ff0d21804d39a309877249af623
Marwa Abouafia <marwaabouafia@gmail.com>
2023-05-10 14:32:05 -0700

    s

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,7 +9,16 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (1);
-	return (wildcmp("hjk", "jkj"));
-
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
 }
