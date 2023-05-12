b620dd988de3914af740122519775a11d351a2f0
Marwa Abouafia <marwaabouafia@gmail.com>
2023-05-12 13:44:53 -0700

    d

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * main - entry
+ *
+ * @argc: int argument
+ * @argv: pointer to array of string arguments
+ *
+ * Return: 0
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc > 0)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+	printf("0\n");
+	return (0);
+}
