6dc17e3ae38d17e5327a82ad45d53992f8f3496c
Marwa Abouafia <marwaabouafia@gmail.com>
2023-05-16 13:23:54 -0700

    g

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * **strtow -  function that splits a string into words.
+ *
+ * @str: char
+ *  Return: char
+ *
+ */
+
+char **strtow(char *str)
+{
+	char *array = NULL;
+	unsigned int i = 0, j = 0, k;
+
+	if (strncmp(str, "", 1) || str == NULL)
+		return (NULL);
+	array = malloc((i + j + 1) * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		array[k] = str[k];
+	i = k;
+	for (k = 0; k < j; k++)
+	{
+		array[i] = str[k];
+		i++;
+	}
+	array[i] = '\0';
+	return (NULL);
+}
