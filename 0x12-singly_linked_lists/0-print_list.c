9d1fba3d1cd2748573f694c448e12ea943e3317a
marwaabouafia <marwaabouafia@gmail.com>
2023-06-01 11:15:36 -0700

    yarab

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,11 +18,17 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[%d] %s\n", 0, "(nil)");
+		}
 		else
+		{
 			printf("[%d] %s\n", h->len, h->str);
-		s++;
+		}
+
 		h = h->next;
+
+		s++;
 	}
 	return (s);
 }
