89ac5a0e5d6424316e03fcae29b74ee70f8adba4
marwaabouafia <marwaabouafia@gmail.com>
2023-06-05 13:20:04 -0700

    103

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -12,14 +12,20 @@
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *holder;
 
-	while (head != NULL)
+	listint_t *current;
+
+	current = head;
+	if (head == NULL)
+		return (NULL);
+
+
+	while (current)
 	{
-		holder = head;
-		head = head->next;
-		if (holder < head)
-			return (head);
+		if (current->next >= current && current->next != NULL)
+			return (current->next);
+
+		current = current->next;
 	}
 	return (NULL);
 }
