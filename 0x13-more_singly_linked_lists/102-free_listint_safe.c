d2f9d39cabee384a405b9e68f20b3e729dc39161
marwaabouafia <marwaabouafia@gmail.com>
2023-06-05 13:08:33 -0700

    102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,16 +10,35 @@
  *
  */
 
-listint_t *find_listint_loop(listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
-	listint_t *holder;
+	listint_t *current;
+	void *addr_prev, *addr_actual;
+	unsigned int counter = 0;
+	int flag = 0;
 
-	while (head != NULL)
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	addr_prev = (void *)*h;
+	while (*h)
 	{
-		holder = head;
-		head = head->next;
-		if (holder < head)
-			return (head);
+		addr_actual = (void *)*h;
+		if (addr_actual >= addr_prev && flag == 1)
+		{
+			*h = NULL;
+			h = NULL;
+			return (counter);
+		}
+		current = (*h)->next;
+		(*h)->next = NULL;
+		free(*h);
+		*h = current;
+		addr_prev = addr_actual;
+		flag = 1;
+		counter++;
 	}
-	return (NULL);
+	h = NULL;
+
+	return (counter);
 }
