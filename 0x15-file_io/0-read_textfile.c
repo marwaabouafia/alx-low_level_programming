1c2d8859e714f039d3a49bf544053b6d94d3625a
marwaabouafia <marwaabouafia@gmail.com>
2023-06-12 13:27:53 -0700

    0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+  * read_textfile - function that reads a text file
+  * and prints it to the POSIX standard output
+  *
+  * @filename: Name of the file.
+  * @letters: Number of letters.
+  *
+  * Return: Always 0.
+  *
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, tmp, out;
+	char *content;
+
+	if (filename == NULL)
+		return (0);
+
+	content = malloc(letters * sizeof(char));
+	if (content == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	tmp = read(fd, content, letters);
+	if (tmp == -1)
+	{
+		close(fd);
+		free(content);
+		return (0);
+	}
+
+	out = write(STDOUT_FILENO, content, tmp);
+	if (out == -1)
+	{
+		close(fd);
+		free(content);
+		return (0);
+	}
+	close(fd);
+	free(content);
+
+	return (out);
+}
